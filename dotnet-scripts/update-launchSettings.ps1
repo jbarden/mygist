415cbc972f61c8990f75521f75789a5180a88565
jbarden <jason.barden@outlook.com>
2024-04-19 14:05:45 +0100

    Fix depth issue on ConvertFrom/To-Json

--- dotnet-scripts/update-launchSettings.ps1
+++ dotnet-scripts/update-launchSettings.ps1
@@ -1,6 +1,6 @@
 [CmdletBinding()]
 Param (
-    [Parameter(Mandatory = $true, HelpMessage = 'Specify the root directory to use to create the new solution.')]
+    [Parameter(Mandatory = $true, HelpMessage = 'Specify the root directory to the launchSettings.json.')]
     [string]$ProjectFolder
 )
 
@@ -14,16 +14,16 @@ process {
     $filePath = "$($ProjectFolder)\Properties\launchSettings.json"
     Write-Output "Updating the $($filePath) file." | WriteColour("DarkMagenta")
 
-    $fileContent = Get-Content -Path $filePath | ConvertFrom-Json
-    
+    $fileContent = Get-Content -Path $filePath | ConvertFrom-Json -Depth 10
+
     $fileContent.PSObject.Properties.Remove('iisSettings')
     foreach ($currentItemName in $($fileContent.profiles)) {
         $currentItemName.PSObject.Properties.Remove('http')
         $currentItemName.PSObject.Properties.Remove('IIS Express')
         $currentItemName.https.applicationUrl = $currentItemName.https.applicationUrl.split(";")[0]
     }
-    
-    $fileContent = $fileContent | ConvertTo-Json
+
+    $fileContent = $fileContent | ConvertTo-Json -Depth 10
     $fileContent | Set-Content -Path $filePath
     Write-Output "Updated the $($filePath) file." | WriteColour("Green")
 }
