1c4d9b0b6b5eab1f3cc4ade88335abb9251546ed
Jason Barden <jason.barden@outlook.com>
2024-10-14 21:56:42 +0100

    add gpg config

--- dotnet-scripts/create-class-library-solution.ps1
+++ dotnet-scripts/create-class-library-solution.ps1
@@ -38,7 +38,14 @@ process{
         mkdir "$($RootDirectory)\$($SolutionNameAsPath)"
         Copy-Item ..\.gitignore -Destination "$($RootDirectory)\$($SolutionNameAsPath)"
         Set-Location "$($RootDirectory)\$($SolutionNameAsPath)"
+        
         git init --initial-branch=main
+        git config --global gpg.program "c:/Program Files (x86)/GnuPG/bin/gpg.exe"
+        git config --global user.signingkey AF697941C147E382
+        git config --global user.name "Jason Barden"
+        git config --global user.email "jason.barden@outlook.com"
+        git config --global commit.gpgsign true
+
         mkdir "$($RootDirectory)\$($SolutionNameAsPath)\src"
         mkdir "$($RootDirectory)\$($SolutionNameAsPath)\tests\unit"
         
